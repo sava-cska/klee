d99fea057307009442640f52b480b7489659262d
Alex Babushkin <ocelaiwo@gmail.com>
2022-04-28 14:27:01 +0500

    add summary printing

--- lib/Core/Summary.cpp
+++ lib/Core/Summary.cpp
@@ -1,6 +1,7 @@
 #include "Summary.h"
 #include "klee/ADT/Ref.h"
 #include "klee/Expr/Expr.h"
+#include <iostream>
 
 using namespace klee;
 
@@ -17,12 +18,17 @@ void Summary::summarize(const Path& path, ProofObligation *pob,
   if(!metadata.queryValidityCore) {
     return;
   }
+  std::cout << "Summary for pob at: " << pob->location->getIRLocation() << std::endl;
   auto core = *metadata.queryValidityCore;
   auto& lemma = lemmas[pob];
+  std::cout << "Constraints are:" << std::endl;
   for(auto constraint : core) {
     if(metadata.rebuildMap.count(constraint.first)) {
       lemma.constraints.push_back(Expr::createIsZero(metadata.rebuildMap.at(constraint.first)));
+      std::cout << lemma.constraints.back()->print() << std::endl;
     }
   }
+  std::cout << "Path is:" << std::endl;
+  std::cout << path.print() << std::endl;
   lemma.paths.push_back(path);
 }

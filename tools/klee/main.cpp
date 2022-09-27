b9c00918f97247cdc376c462beafe8efbe106b0a
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2022-09-27 11:08:51 +0300

    [fix] Fixed include

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -39,6 +39,7 @@
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Host.h"
 
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/Signals.h"
